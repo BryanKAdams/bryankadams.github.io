d3966c88f64eb196cd8494e79f76a4b85b3a464e
unknown <bryankeltonadams@gmail.com>
2020-05-22 13:58:50 -0600

    Changed default import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/button'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
+import {Button} from '@material-ui/core/'
+import {Typography} from '@material-ui/core/'
 
 function Header() {
     return (
